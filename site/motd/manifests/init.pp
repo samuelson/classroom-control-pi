66bdd9c7b5cefb00682103a32ef8211968e70226
Josh Samuelson <js@puppetlabs.com>
2016-06-10 15:30:47 -0700

    Update init.pp

--- site/motd/manifests/init.pp
+++ site/motd/manifests/init.pp
@@ -2,6 +2,7 @@ class motd {
   exec { "figlet 'Welcome to ${::fqdn}!' > /etc/motd":
     path    => '/bin:/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
+    require => Package['figlet'],
   }
 
   package { 'figlet':
