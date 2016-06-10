59c3c8c5c988e1b9f1728149ab090d2074af46d9
Josh Samuelson <js@puppetlabs.com>
2016-06-10 16:02:55 -0700

    Update init.pp

--- site/dsciis/manifests/init.pp
+++ site/dsciis/manifests/init.pp
@@ -1,68 +1,62 @@
 class dsciis {
-
-  # TODO: add the correct name to this resource so DSC can trigger reboots
-  reboot { '???':
-    when    => pending,
-    timeout => 15,
-  }
-
-  package { 'dotnet4.5.2':
-    ensure   => latest,
-    provider => 'chocolatey',
-    notify   => Reboot['dsc_reboot'],
-  }
-
-  vcsrepo { 'C:/inetpub/puppetize':
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/puppetlabs-education/asp-starter-site.git',
-  }
-
-  # Translate the commented DSC Powershell resources into Puppet code below
-  # Add relationship chaining arrows between each resource
-#  WindowsFeature iis {
-#    Ensure    = 'Present'
-#    Name      = 'Web-Server'
-#  }
-#  WindowsFeature iisscriptingtools {
-#    Ensure    = 'Present',
-#    Name      = 'Web-Scripting-Tools',
-#  }
-#  WindowsFeature aspnet45 {
-#    Ensure    = 'Present'
-#    Name      = 'Web-Asp-Net45'
-#    DependsOn = @("[WindowsFeature]iis")
-#  }
-  -> dsc_xwebsite{'defaultsite':
-    dsc_ensure       => 'Present',
-    dsc_name         => 'Default Web Site',
-    dsc_state        => 'Stopped',
-    dsc_physicalpath => 'C:\inetpub\wwwroot',
-  }
-  -> dsc_xwebapppooldefaults{'newwebapppool':
-    dsc_managedruntimeversion     => 'v4.0',
-    dsc_identitytype              => 'ApplicationPoolIdentity',
-    dsc_applyto                   => 'Machine',
-  }
-  -> dsc_xwebapppool{'newwebapppool':
-    dsc_name                      => 'PuppetCodezAppPool',
-    dsc_ensure                    => 'Present',
-    dsc_state                     => 'Started',
-  }
-  -> dsc_xwebsite{'newwebsite':
-    dsc_ensure          => 'Present',
-    dsc_name            => 'PuppetCodez',
-    dsc_state           => 'Started',
-    dsc_physicalpath    => 'C:\inetpub\puppetize',
-    dsc_applicationpool => 'PuppetCodezAppPool',
-    dsc_bindinginfo     => [{
-      protocol => 'HTTP',
-      port     => 80,
-    }],
-    require             => [
-#      Vcsrepo[???],                     # add the relationship reference titles
-#      Package[???],
-    ],
-  }
-
+# Corrected name to this resource so DSC can trigger reboots
+reboot { 'dsc_reboot':
+when => pending,
+timeout => 15,
+}
+package { 'dotnet4.5.2':
+ensure => latest,
+provider => 'chocolatey',
+notify => Reboot['dsc_reboot'],
+}
+vcsrepo { 'C:/inetpub/puppetize':
+ensure => present,
+provider => git,
+source => 'https://github.com/puppetlabs-education/asp-starter-site.git',
+revision => 'production',
+}
+# Translated the provided DSC Powershell script into Puppet code below
+dsc_windowsfeature{'iis':
+dsc_ensure => 'Present',
+dsc_name => 'Web-Server',
+}
+-> dsc_windowsfeature{'iisscriptingtools':
+dsc_ensure => 'Present',
+dsc_name => 'Web-Scripting-Tools',
+}
+-> dsc_windowsfeature{'aspnet45':
+dsc_ensure => 'Present',
+dsc_name => 'Web-Asp-Net45',
+}
+-> dsc_xwebsite{'defaultsite':
+dsc_ensure => 'Present',
+dsc_name => 'Default Web Site',
+dsc_state => 'Stopped',
+dsc_physicalpath => 'C:\inetpub\wwwroot',
+}
+-> dsc_xwebapppooldefaults{'newwebapppool':
+dsc_managedruntimeversion => 'v4.0',
+dsc_identitytype => 'ApplicationPoolIdentity',
+dsc_applyto => 'Machine',
+}
+-> dsc_xwebapppool{'newwebapppool':
+dsc_name => 'PuppetCodezAppPool',
+dsc_ensure => 'Present',
+dsc_state => 'Started',
+}
+-> dsc_xwebsite{'newwebsite':
+dsc_ensure => 'Present',
+dsc_name => 'PuppetCodez',
+dsc_state => 'Started',
+dsc_physicalpath => 'C:\inetpub\puppetize',
+dsc_applicationpool => 'PuppetCodezAppPool',
+dsc_bindinginfo => [{
+protocol => 'HTTP',
+port => 80,
+}],
+require => [
+Vcsrepo['C:/inetpub/puppetize'], # added dependencies
+Package['dotnet4.5.2'],
+],
+}
 }
