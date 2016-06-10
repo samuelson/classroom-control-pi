1d3b27af074e5e7d99173eb4e064295f2d056439
Josh Samuelson <js@puppetlabs.com>
2016-06-10 15:02:46 -0700

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,6 +1,14 @@
 class profile::redis {
   include profile::epel
-  class {'redis':
-    maxmemory => '10mb',
+  if $master {
+    class {'redis':
+      maxmemory => '10mb',
+    }
+  } else {
+    class {'redis':
+      maxmemory => '10mb',
+      bind => $ipaddress,
+      slaveof => 'master.puppetlabs.vm 6479',
+    }
   }
 }
